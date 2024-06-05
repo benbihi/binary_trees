3a3523e81df5585485f503a34ec81c9c69ae515b
benbihi <elm.benbihi@gmail.com>
2024-06-05 12:09:25 -0700

    task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - create a new right node
+ *
+ * @parent: the parent node
+ * @value: the node value
+ * Return: pointer to the node
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+        binary_tree_t *newNode;
+
+        if (parent == NULL)
+        {
+                return (NULL);
+        }
+        newNode = binary_tree_node(parent, value);
+        if (parent->right == NULL)
+        {
+                parent->right = newNode;
+        }
+        else
+        {
+                newNode->right = parent->right;
+                parent->right->parent = newNode;
+                parent->right = newNode;
+        }
+        return (newNode);
+}
